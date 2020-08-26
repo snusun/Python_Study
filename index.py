53c1253ac7d333a23bf26c360f8f396c3f414f8c
Sunyoung <0515hwang@snu.ac.kr>
2020-08-26 14:26:38 +0900

    Add python files

---
+++ index.py
@@ -0,0 +1,42 @@
+#!python
+print("content-type: text/html; charset=utf-8\n")
+print()
+
+import cgi, os, view
+
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    #delete is not a link(avoid drawbacks), it's a form
+    delete_action = '''
+        <form action="process_delete.py" method="post">
+            <input type="hidden" name="pageId" value={}>
+            <input type="submit" value="delete">
+        </form>
+    '''.format(pageId)
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+    update_link = ''
+    delete_action = ''
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+  {update_link}
+  {delete_action}
+  <h2>{title}</h2>
+  <p>{desc}</p>
+</body>
+</html>
+'''.format(title=pageId, desc=description, listStr=view.getList(), update_link=update_link, delete_action=delete_action))
---
+++ update.py
@@ -0,0 +1,34 @@
+#!python
+print("content-type: text/html; charset=utf-8\n")
+print()
+
+import cgi, os, view
+
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId, 'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="update.py">update</a>
+  <form action="process_update.py" method="post">
+      <input type="hidden" name="pageId" value={form_default_title}>
+      <p><input type="text" name="title" value={form_default_title}></p>
+      <p><textarea rows="4" name="description" placeholder="description">{form_default_description}</textarea></p>
+      <p><input type="submit"></p>
+  </form>
+</body>
+</html>
+'''.format(title=pageId, desc=description, listStr=view.getList(), form_default_title=pageId, form_default_description=description))
